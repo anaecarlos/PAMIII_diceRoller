9756c5c3b81474e9fa1e2755cbb2925042b932ce
Ana e Tevez <anatevezvitoriarosseto@gmail.com>
2024-06-21 17:44:39 -0300

    DiceRoller

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace diceRoller
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace diceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RollDiceButton_Clicked(object sender, EventArgs e)
+        {
+            if (SidesPicker.SelectedItem != null)
+            {
+                int selectSides = Convert.ToInt32(SidesPicker.SelectedItem);
+
+                Random random = new Random();
+                int drawnNumber = random.Next(1, selectSides + 1);
+
+                ResultLabel.Text = drawnNumber.ToString();
+            }
+            else
+            {
+                ResultLabel.Text = "Selecione o numero de lados.";
+            }
+        }
+    }
+}
